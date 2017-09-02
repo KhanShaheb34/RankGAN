4c6c7b83998da638b6f653e33917fcd32d3ee2bb
desire2020 <desire2020@126.com>
2017-09-02 18:43:51 +0800

    NEWS data.

--- model_settings.py
+++ model_settings.py
@@ -1,5 +1,7 @@
-seq_len = 32
 use_real_data = True
 COCO_vocab_size = 4980
+COCO_seq_len = 32
 NEWS_vocab_size = 5742
-real_data_vocab_size = COCO_vocab_size
+NEWS_seq_len = 48
+seq_len = NEWS_seq_len
+real_data_vocab_size = NEWS_vocab_size
